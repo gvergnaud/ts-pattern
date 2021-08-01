1d481333226d946c5a866d028b3010c3b86712d5
gvergnaud <gabriel.vergnaud@gmail.com>
2021-08-01 14:14:10 +0200

    Format otherwise.test.ts

--- tests/otherwise.test.ts
+++ tests/otherwise.test.ts
@@ -1,10 +1,10 @@
 import { match, __ } from '../src';
 
 describe('otherwise', () => {
-    it('should pass matched value to otherwise', () => {
-        const result = match(42)
-            .with(51, d => d)
-            .otherwise(d => d);
-        expect(result).toBe(42);
-    })
+  it('should pass matched value to otherwise', () => {
+    const result = match(42)
+      .with(51, (d) => d)
+      .otherwise((d) => d);
+    expect(result).toBe(42);
+  });
 });
