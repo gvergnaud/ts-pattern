8e506d5b2e3716fa7b40af91c22320f6cf0b0198
gvergnaud <gabriel.vergnaud@gmail.com>
2022-04-16 22:19:40 +0200

    Improve runtime perfs of 'match'
    by using a Builder class instead of returning objects

--- src/match.ts
+++ src/match.ts
@@ -24,106 +24,108 @@ import { matchPattern } from './internals/helpers';
  */
 export const match = <input, output = symbols.unset>(
   value: input
-): Match<input, output> => builder(value, []) as any;
+): Match<input, output> => new Builder(value, []) as any;
 
-const builder = <i, o>(
-  value: i,
-  cases: {
-    test: (value: i) => unknown;
-    select: (value: i) => any;
-    handler: (...args: any) => any;
-  }[]
-) => {
-  const run = () => {
-    const entry = cases.find(({ test }) => test(value));
+class Builder<i, o> {
+  constructor(
+    private value: i,
+    private cases: {
+      test: (value: i) => unknown;
+      select: (value: i) => any;
+      handler: (...args: any) => any;
+    }[]
+  ) {}
+
+  with(...args: any[]) {
+    const handler = args[args.length - 1];
+
+    const patterns: Pattern<i>[] = [];
+    const predicates: ((value: i) => unknown)[] = [];
+
+    // case with guard as second argument
+    if (args.length === 3 && typeof args[1] === 'function') {
+      patterns.push(args[0]);
+      predicates.push(args[1]);
+    } else {
+      patterns.push(...args.slice(0, args.length - 1));
+    }
+
+    let selected: Record<string, unknown> = {};
+
+    const doesMatch = (value: i) =>
+      Boolean(
+        patterns.some((pattern) =>
+          matchPattern(pattern, value, (key, value) => {
+            selected[key] = value;
+          })
+        ) && predicates.every((predicate) => predicate(value as any))
+      );
+
+    return new Builder(
+      this.value,
+      this.cases.concat([
+        {
+          test: doesMatch,
+          handler,
+          select: (value) =>
+            Object.keys(selected).length
+              ? symbols.anonymousSelectKey in selected
+                ? selected[symbols.anonymousSelectKey]
+                : selected
+              : value,
+        },
+      ])
+    );
+  }
+
+  when<p extends (value: i) => unknown, c>(
+    predicate: p,
+    handler: (value: GuardValue<p>) => PickReturnValue<o, c>
+  ) {
+    return new Builder<i, PickReturnValue<o, c>>(
+      this.value,
+      this.cases.concat([
+        {
+          test: predicate,
+          handler,
+          select: (value) => value,
+        },
+      ])
+    );
+  }
+
+  otherwise<c>(
+    handler: (value: i) => PickReturnValue<o, c>
+  ): PickReturnValue<o, c> {
+    return new Builder<i, PickReturnValue<o, c>>(
+      this.value,
+      this.cases.concat([
+        {
+          test: () => true,
+          handler,
+          select: (value) => value,
+        },
+      ])
+    ).run();
+  }
+
+  exhaustive() {
+    return this.run();
+  }
+
+  run() {
+    const entry = this.cases.find(({ test }) => test(this.value));
     if (!entry) {
       let displayedValue;
       try {
-        displayedValue = JSON.stringify(value);
+        displayedValue = JSON.stringify(this.value);
       } catch (e) {
-        displayedValue = value;
+        displayedValue = this.value;
       }
       throw new Error(
         `Pattern matching error: no pattern matches value ${displayedValue}`
       );
     }
-    return entry.handler(entry.select(value), value);
-  };
-
-  return {
-    with(...args: any[]) {
-      const handler = args[args.length - 1];
-
-      const patterns: Pattern<i>[] = [];
-      const predicates: ((value: i) => unknown)[] = [];
-
-      // case with guard as second argument
-      if (args.length === 3 && typeof args[1] === 'function') {
-        patterns.push(args[0]);
-        predicates.push(args[1]);
-      } else {
-        patterns.push(...args.slice(0, args.length - 1));
-      }
-
-      let selected: Record<string, unknown> = {};
-
-      const doesMatch = (value: i) =>
-        Boolean(
-          patterns.some((pattern) =>
-            matchPattern(pattern, value, (key, value) => {
-              selected[key] = value;
-            })
-          ) && predicates.every((predicate) => predicate(value as any))
-        );
-
-      return builder(
-        value,
-        cases.concat([
-          {
-            test: doesMatch,
-            handler,
-            select: (value) =>
-              Object.keys(selected).length
-                ? symbols.anonymousSelectKey in selected
-                  ? selected[symbols.anonymousSelectKey]
-                  : selected
-                : value,
-          },
-        ])
-      );
-    },
-
-    when: <p extends (value: i) => unknown, c>(
-      predicate: p,
-      handler: (value: GuardValue<p>) => PickReturnValue<o, c>
-    ) =>
-      builder<i, PickReturnValue<o, c>>(
-        value,
-        cases.concat([
-          {
-            test: predicate,
-            handler,
-            select: (value) => value,
-          },
-        ])
-      ),
-
-    otherwise: <c>(
-      handler: (value: i) => PickReturnValue<o, c>
-    ): PickReturnValue<o, c> =>
-      builder<i, PickReturnValue<o, c>>(
-        value,
-        cases.concat([
-          {
-            test: () => true,
-            handler,
-            select: (value) => value,
-          },
-        ])
-      ).run(),
-
-    exhaustive: () => run(),
-
-    run,
-  };
-};
+    return entry.handler(entry.select(this.value), this.value);
+  }
+}
