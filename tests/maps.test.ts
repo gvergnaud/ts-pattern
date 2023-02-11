924a9221f913b5bfd273dfae3013be612f16ea68
gvergnaud <gabriel.vergnaud@gmail.com>
2023-02-11 20:29:53 +0100

    set & map: allow no arguments

--- tests/maps.test.ts
+++ tests/maps.test.ts
@@ -4,22 +4,35 @@ import { match, P } from '../src';
 describe('Map', () => {
   it('should match Map patterns', () => {
     const usersMap = new Map([
-      ['gab', { name: 'gabriel' }],
-      ['angégé', { name: 'angéline' }],
+      ['a', { name: 'alice' }],
+      ['b', { name: 'bob' }],
     ]);
 
     const userPattern = { name: P.string };
 
     const res = match<Map<string, { name: string }>>(usersMap)
-      .with(P.map(P.union('angégé', 'gab'), userPattern), (map) => ({
-        name: map.get('angégé')!.name + ' ' + map.get('gab')!.name,
+      .with(P.map(P.union('b', 'a'), userPattern), (map) => ({
+        name: map.get('b')!.name + ' ' + map.get('a')!.name,
       }))
-      .with(P.map('angégé', userPattern), (map) => map.get('angégé')!)
+      .with(P.map('b', userPattern), (map) => map.get('b')!)
       .with(P._, () => ({ name: 'unknown' }))
       .run();
 
     type t = Expect<Equal<typeof res, { name: string }>>;
 
-    expect(res).toEqual({ name: 'angéline gabriel' });
+    expect(res).toEqual({ name: 'bob alice' });
+  });
+
+  it("should match any map if P.map isn't given any arguments", () => {
+    const usersMap = new Map([
+      ['a', { name: 'alice' }],
+      ['b', { name: 'bob' }],
+    ]);
+
+    const res = match<Map<string, { name: string }>>(usersMap)
+      .with(P.map(), () => true)
+      .exhaustive();
+    type t = Expect<Equal<typeof res, boolean>>;
+    expect(res).toEqual(true);
   });
 });
