d88759d8b88305ac3c692b6af6093427caef9618
cuppachino <cuppachino.dev@gmail.com>
2023-02-19 01:05:46 -0600

    remap extensions in jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,11 @@
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {'\\.[jt]sx?$': ['ts-jest', { useESM: true }] },
+  moduleNameMapper: {
+    '(.+)\\.js': '$1',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.mts']
 };
