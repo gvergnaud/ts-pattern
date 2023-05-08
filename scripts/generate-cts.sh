26d430a87c5eef3e3b1fd24f5fee4e3a1dd33639
gvergnaud <gabriel.vergnaud@gmail.com>
2023-05-08 11:15:57 +0200

    build: Include extensions in esm type declaration files

--- scripts/generate-cts.sh
+++ scripts/generate-cts.sh
@@ -6,4 +6,6 @@ files=$(find dist -type f -name "*.d.ts");
 for file in $files; do
     # Update imports to include the '.cjs' extension
     sed -E "s/(.*)from '([^']*)'/\1from '\2.cjs'/g" "$file" > "${file%.d.ts}.d.cts"
+    # add `.js` extensions to .d.ts files
+    sed -i '' -e "s/\(.*\)from '\([^']*\)'/\1from '\2.js'/g" "$file"
 done
